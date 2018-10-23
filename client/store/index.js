7adc9e757d74ab15cbed4b1e4ee6dd1861c14fb6
Danny <Lorddanp@aol.com>
2018-10-23 16:35:43 -0400

    Feat(reducer): implements allDevs and singleDev into combineReducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './user';
+import allDeveloper from './singleDeveloper';
+import allDevelopers from './allDevelopers';
 
-const reducer = combineReducers({ user });
+const reducer = combineReducers({ user, allDeveloper, allDevelopers });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
